8c081e85944a269002271ef907369f1264ed1c97
Anthony <33587293+JetCrazy@users.noreply.github.com>
2019-04-05 09:13:38 -0400

    Update Driver.java

--- java/Population/Driver.java
+++ java/Population/Driver.java
@@ -7,16 +7,27 @@ import java.util.Scanner;
  */
 public class Driver
 {
-   public static void main() {
    Scanner input = new Scanner(System.in);
-   System.out.println("Enter the amount of starting organisms");
-   int orgs = input.nextInt();
-   System.out.println("Enter the Average Increase");
-   double avg = input.nextDouble();
-   System.out.println("Enter the amount of days");
-   int day = input.nextInt();
-   
-   Population pop = new Population(orgs, avg, day);
-   System.out.println(pop.calculate());
-}
+		   System.out.println("Enter the amount of starting organisms");
+		   int orgs = input.nextInt();
+		   while (orgs < 2) {
+	            System.out.println("Please Enter a valid number amount of Organisms");
+	            orgs = input.nextInt();
+	        }
+		   System.out.println("Enter the Average Increase");
+		   double avg = input.nextDouble();
+		   while (avg < 0) {
+	            System.out.println("Please Enter a valid AVG");
+	            avg = input.nextInt();
+	        }
+		   System.out.println("Enter the amount of days");
+		   int day = input.nextInt();
+		   while (day < 1) {
+	            System.out.println("Please Enter a valid number amount of Days");
+	            day = input.nextInt();
+	        }
+		   
+		   Population pop = new Population(orgs, avg, day);
+		   System.out.println(pop.calculate());
+		}
 }
