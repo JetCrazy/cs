f849ad26692945819f8173ee1d138338dc4899a2
Anthony <33587293+JetCrazy@users.noreply.github.com>
2019-04-11 14:21:23 -0400

    Update Driver.java

--- java/lottery/Driver.java
+++ java/lottery/Driver.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
 /**
+ * Write a description of class Driver here.
  *
- * @author jetcr
+ * @author (your name)
+ * @version (a version number or a date)
  */
-public class Driver {
+public class Driver
+{
     public static void main() {
         Scanner input = new Scanner(System.in);
         int[] userNumbers = new int[5];
-        
         int temp = 0;
+        for(int i = 0; i < userNumbers.length; i++) {
+            System.out.println("Please enter number 0-9");
+            temp = input.nextInt();
+            userNumbers[i] = temp;
+        }
+        
+        Lottery lottery = new Lottery();
+        System.out.println(lottery.compare(userNumbers));
+        System.out.println(lottery.getLotteryNumbers());
+        System.out.println("main(): end of program");
+    }
+}
+
+
+/*int temp = 0;
         for(int i = 0; i < userNumbers.length; i++) {
             System.out.printf("Please enter a number 0-9\n");
             temp = input.nextInt();
@@ -23,7 +40,4 @@ public class Driver {
                     i--; // if a number was the same subtract the iteration counter and restart the loop
                 }
             }
-        }
-        
-    }
-}
+        } // end of first for loop*/
